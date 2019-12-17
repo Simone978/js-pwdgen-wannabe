b0c7d7682acf3727ce7b4494c5bcd03040500363
Simone <simonelega.sl@gmail.com>
2019-12-17 15:50:34 +0100

    password generata inserita nel tag span

--- js/generator.js
+++ js/generator.js
@@ -10,3 +10,6 @@ var numFisso = 19;
 // creo la password tramite concatenazione dei valori delle variabili
 var password = nome + cognome + colore + numFisso;
 // console.log(password);
+
+// inserisco la password generata all'interno dello span con id Password
+document.getElementById('password').innerHTML = password;
