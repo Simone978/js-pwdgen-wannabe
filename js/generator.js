8a084737af2480a07aade1ed2824b49b637177aa
Simone <simonelega.sl@gmail.com>
2019-12-17 15:44:06 +0100

    Create variabili e verifica con console.log

--- js/generator.js
+++ js/generator.js
@@ -1,1 +1,12 @@
-alert('ciao');
+// chiedo all'utente il suo nome, cognome e colore preferito
+var nome = prompt("Qual è il tuo nome?");
+// console.log(nome);
+var cognome = prompt("Ora dimmi il tuo cognome?");
+// console.log(cognome);
+var colore = prompt("Qual è il tuo colore preferito?");
+// console.log(colore);
+var numFisso = 19;
+
+// creo la password tramite concatenazione dei valori delle variabili
+var password = nome + cognome + colore + numFisso;
+// console.log(password);
